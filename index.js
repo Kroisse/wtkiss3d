f30ad55d6199cd97eddf4fc355eec2302a2a1e9d
Eunchong Yu <kroisse@gmail.com>
2020-10-28 09:43:30 +0900

    Add some UI

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 async function main({ init, load_gltf }) {
     const engine = init();
-    const gltf = await load_gltf('');
-    engine.add_gltf(gltf);
+
+    const inputBox = document.getElementById('gltf-url');
+    const gltfForm = document.getElementById('load-gltf');
+
+    gltfForm.addEventListener('submit', async e => {
+        e.preventDefault();
+        e.target.disabled = true;
+        try {
+            const gltf = await load_gltf(inputBox.value);
+            engine.add_gltf(gltf);
+        } finally {
+            e.target.disabled = false;
+        }
+    });
 }
 
 import('./pkg')
